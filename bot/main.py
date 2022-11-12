61d5f65c030ad9b4293ebed5c3b16413d675b0ff
Zacharino <99297912+Zacharino@users.noreply.github.com>
2022-11-12 13:54:14 +0100

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -40,114 +40,114 @@ async def boss(ctx, arg):
   boss_name = ''
   thumbnail = ''
   #if(re.search(p, arg)):
-    author_id = ctx.message.author.id
-    try:
-      boss_value = arg.split('_')[1]
-      print(boss_value)
-    except:
-      ctx.send("Pls use dps.report link")
-    else:
-      kill_date_raw = arg.split('-')[1].split('_')[0]
-      kill_date = datetime.strptime(kill_date_raw, "%Y%m%d").date()
-      if boss_value in consts:
-        boss_name = consts.get(boss_value)
-        thumbnail = consts.get(boss_value + '-thumb')
+  author_id = ctx.message.author.id
+  try:
+    boss_value = arg.split('_')[1]
+    print(boss_value)
+  except:
+    ctx.send("Pls use dps.report link")
+  else:
+    kill_date_raw = arg.split('-')[1].split('_')[0]
+    kill_date = datetime.strptime(kill_date_raw, "%Y%m%d").date()
+    if boss_value in consts:
+      boss_name = consts.get(boss_value)
+      thumbnail = consts.get(boss_value + '-thumb')
 
-        embed_title = boss_name + ' ' + str(kill_date)
-        embed_msg = discord.Embed(title=embed_title, description='Our RL submission for ' + boss_name + '. \nMake sure to add your PoVs.', colour=0x1c1c1c, url=arg)
-        embed_msg.set_image(url=thumbnail)
-        embed_msg.add_field(name='Roster', value='\u200b', inline=True)
-        embed_msg.add_field(name='PoVs', value='\u200b', inline=True)
+      embed_title = boss_name + ' ' + str(kill_date)
+      embed_msg = discord.Embed(title=embed_title, description='Our RL submission for ' + boss_name + '. \nMake sure to add your PoVs.', colour=0x1c1c1c, url=arg)
+      embed_msg.set_image(url=thumbnail)
+      embed_msg.add_field(name='Roster', value='\u200b', inline=True)
+      embed_msg.add_field(name='PoVs', value='\u200b', inline=True)
 
-        message_id = ''
+      message_id = ''
 
-        class View(discord.ui.View):
+      class View(discord.ui.View):
 
-          # ADD ROSTER BUTTON
-          @discord.ui.button(label='Add Roster', style=discord.ButtonStyle.green)
-          async def roster_button_callback(self, interaction: discord.Interaction, button: discord.ui.Button):
+        # ADD ROSTER BUTTON
+        @discord.ui.button(label='Add Roster', style=discord.ButtonStyle.green)
+        async def roster_button_callback(self, interaction: discord.Interaction, button: discord.ui.Button):
 
-            old_roster_value = embed_msg.fields[0].value
-            await interaction.response.send_message("Please submit the names for the roster: ", ephemeral=True)
+          old_roster_value = embed_msg.fields[0].value
+          await interaction.response.send_message("Please submit the names for the roster: ", ephemeral=True)
 
-            def check(m):
-              return m.author.id == author_id and m.channel.id == message_channel
-            
-            msg = await bot.wait_for("message", check=check)
-            msg_content = msg.content
-            joined_msg = "".join(msg_content)
-            formatted_msg = joined_msg.replace(' ', '\n')
-            if old_roster_value == "\u200b":
-              new_roster_value = formatted_msg
-            else:
-              new_roster_value = old_roster_value + "\n" + formatted_msg
-            
-            embed_msg.set_field_at(0, name="Roster", value=new_roster_value, inline=True)
-            await interaction.edit_original_response(content="Successfully added the roster!")
+          def check(m):
+            return m.author.id == author_id and m.channel.id == message_channel
+
+          msg = await bot.wait_for("message", check=check)
+          msg_content = msg.content
+          joined_msg = "".join(msg_content)
+          formatted_msg = joined_msg.replace(' ', '\n')
+          if old_roster_value == "\u200b":
+            new_roster_value = formatted_msg
+          else:
+            new_roster_value = old_roster_value + "\n" + formatted_msg
+
+          embed_msg.set_field_at(0, name="Roster", value=new_roster_value, inline=True)
+          await interaction.edit_original_response(content="Successfully added the roster!")
+          await message.edit(embed=embed_msg)
+          await msg.delete()
+
+
+        # ADD POV BUTTON
+        @discord.ui.button(label='Add PoV', style=discord.ButtonStyle.green)
+        async def pov_button_callback(self, interaction: discord.Interaction, button: discord.ui.Button):
+
+          old_pov_value = embed_msg.fields[1].value
+          await interaction.response.send_message("Please submit your PoV: ", ephemeral=True)
+
+          def check(m):
+            return m.author.id == author_id and m.channel.id == message_channel
+
+          msg = await bot.wait_for("message", check=check)
+          msg_content = msg.content
+          formatted_msg = "".join(msg_content)
+          if old_pov_value == "\u200b":
+            new_pov_value = formatted_msg
+          else:
+            new_pov_value = old_pov_value + "\n" + formatted_msg
+
+          embed_msg.set_field_at(1, name="PoVs", value=new_pov_value, inline=True)
+          await interaction.edit_original_response(content="Successfully added PoV!")
+          await message.edit(embed=embed_msg)
+          await msg.delete()
+
+
+        # REMOVE ROSTER BUTTON
+        @discord.ui.button(label='Remove Roster', style=discord.ButtonStyle.red)
+        async def delete_roster_button_callback(self, interaction: discord.Interaction, button: discord.ui.Button):
+          if(embed_msg.fields[0].value == '\u200b'):
+            await interaction.response.send_message("Nothing to remove you monkey!", ephemeral=True)
+          else:
+            await interaction.response.send_message("Roster removed successfully!", ephemeral=True)
+            embed_msg.set_field_at(0, name="Roster", value='\u200b', inline=True)
             await message.edit(embed=embed_msg)
-            await msg.delete()
 
+        # REMOVE POV BUTTON
+        @discord.ui.button(label='Remove PoV', style=discord.ButtonStyle.red)
+        async def delete_pov_button_callback(self, interaction: discord.Interaction, button: discord.ui.Button):
 
-          # ADD POV BUTTON
-          @discord.ui.button(label='Add PoV', style=discord.ButtonStyle.green)
-          async def pov_button_callback(self, interaction: discord.Interaction, button: discord.ui.Button):
-
-            old_pov_value = embed_msg.fields[1].value
-            await interaction.response.send_message("Please submit your PoV: ", ephemeral=True)
-
-            def check(m):
-              return m.author.id == author_id and m.channel.id == message_channel
-            
-            msg = await bot.wait_for("message", check=check)
-            msg_content = msg.content
-            formatted_msg = "".join(msg_content)
-            if old_pov_value == "\u200b":
-              new_pov_value = formatted_msg
-            else:
-              new_pov_value = old_pov_value + "\n" + formatted_msg
-            
+          old_pov_value = embed_msg.fields[1].value
+          old_pov_value_split = old_pov_value.split('\n')
+          if(len(old_pov_value_split)>1):
+            new_pov_value = '\n'.join(old_pov_value_split[:-1])
             embed_msg.set_field_at(1, name="PoVs", value=new_pov_value, inline=True)
-            await interaction.edit_original_response(content="Successfully added PoV!")
             await message.edit(embed=embed_msg)
-            await msg.delete()
+            await interaction.response.send_message("Latest PoV entry removed successfully!", ephemeral=True)
+          elif(len(old_pov_value_split)==1 and old_pov_value_split[0]!='\u200b'):
+            embed_msg.set_field_at(1, name="PoVs", value='\u200b', inline=True)
+            await message.edit(embed=embed_msg)
+            await interaction.response.send_message("Latest PoV entry removed successfully!", ephemeral=True)
+          else:
+            await interaction.response.send_message("Nothing to remove you monkey!", ephemeral=True)
 
 
-          # REMOVE ROSTER BUTTON
-          @discord.ui.button(label='Remove Roster', style=discord.ButtonStyle.red)
-          async def delete_roster_button_callback(self, interaction: discord.Interaction, button: discord.ui.Button):
-            if(embed_msg.fields[0].value == '\u200b'):
-              await interaction.response.send_message("Nothing to remove you monkey!", ephemeral=True)
-            else:
-              await interaction.response.send_message("Roster removed successfully!", ephemeral=True)
-              embed_msg.set_field_at(0, name="Roster", value='\u200b', inline=True)
-              await message.edit(embed=embed_msg)
+      view = View()
+      message = await ctx.send(embed=embed_msg, view=view)
+      message_id = message.id
+      message_channel = message.channel.id
 
-          # REMOVE POV BUTTON
-          @discord.ui.button(label='Remove PoV', style=discord.ButtonStyle.red)
-          async def delete_pov_button_callback(self, interaction: discord.Interaction, button: discord.ui.Button):
-
-            old_pov_value = embed_msg.fields[1].value
-            old_pov_value_split = old_pov_value.split('\n')
-            if(len(old_pov_value_split)>1):
-              new_pov_value = '\n'.join(old_pov_value_split[:-1])
-              embed_msg.set_field_at(1, name="PoVs", value=new_pov_value, inline=True)
-              await message.edit(embed=embed_msg)
-              await interaction.response.send_message("Latest PoV entry removed successfully!", ephemeral=True)
-            elif(len(old_pov_value_split)==1 and old_pov_value_split[0]!='\u200b'):
-              embed_msg.set_field_at(1, name="PoVs", value='\u200b', inline=True)
-              await message.edit(embed=embed_msg)
-              await interaction.response.send_message("Latest PoV entry removed successfully!", ephemeral=True)
-            else:
-              await interaction.response.send_message("Nothing to remove you monkey!", ephemeral=True)
-            
-
-        view = View()
-        message = await ctx.send(embed=embed_msg, view=view)
-        message_id = message.id
-        message_channel = message.channel.id
-      
-      else:
-        await ctx.send('Name not found - ping the fuck out of Zach', delete_after=1)
+    else:
+      await ctx.send('Name not found - ping the fuck out of Zach', delete_after=1)
 
  # else:
     #await ctx.send("Incorrect URL! Please try running the command again with a different URL.", delete_after=1)
